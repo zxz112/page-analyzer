96dd80d75fb8a5b7a68576652b33178cadd9c0d4
Ilya VVV <Ilya@mail.ru>
2020-03-16 18:21:05 +0300

    step5

--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -26,11 +26,10 @@ class DomainController extends Controller
     public function index()
     {
         $last = DB::table('domain_checks')->select('status_code', 'domain_id', 'id')->groupBy('domain_id')->get();
-        dump($last);
         $domains = DB::select('select domains.id, domains.name, max(domain_checks.updated_at) as updated from domains
         left join domain_checks on domains.id = domain_checks.domain_id group by domains.id,  domains.name order by domains.id');
 
-        return view('index', compact('domains'));
+        return view('index', compact(['domains', 'last']));
     }
 
     public function store(Request $request)
