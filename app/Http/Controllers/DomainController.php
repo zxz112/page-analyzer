94d41f845cb37ff26e58c90964d0c70f8cb3c2aa
Ilya VVV <Ilya@mail.ru>
2020-03-16 16:18:12 +0300

    step5

--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -25,7 +25,8 @@ class DomainController extends Controller
 
     public function index()
     {
-        $domains = DB::select('select domains.id, domains.name, max(domain_checks.updated_at) as updated from domains 
+        // $do = DB::select('select id, domain_id, status_code from domain_checks')
+        $domains = DB::select('select domains.id, domains.name, domain_checks.status_code as updated from domains 
         left join domain_checks on domains.id = domain_checks.domain_id group by domains.id, domains.name order by domains.id');
         return view('index', compact('domains'));
     }
